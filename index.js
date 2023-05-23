ce164ba589f3f0895f45380d79cfacf398ac7730
Prakriti Pokharel <iprakritip@gmail.com>
2023-05-23 16:34:49 +0545

    add dolllar sign to write js in string, stupid

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = JSON.parse(localStorage.getItem('items'))||[];
+const items = JSON.parse(localStorage.getItem('items')) || [];
 
 
 function addItem(e) {
@@ -12,35 +12,35 @@ function addItem(e) {
     };
     items.push(item);
     populateList(items, itemsList)
-    localStorage.setItem('items',`${JSON.stringify(items)}`)
+    localStorage.setItem('items', `${JSON.stringify(items)}`)
     this.reset();
 
 }
 
 
-function populateList(plates=[], platesList) {
-    platesList.innerHTML = plates.length>0 && plates.map((plate, i) => {
+function populateList(plates = [], platesList) {
+    platesList.innerHTML = plates.length > 0 && plates.map((plate, i) => {
+        console.log(plate)
         return `
         <li>
-        <input type="checkbox" data-index=${i} id="item${i}" {plate.done? 'checked' : ''} />
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done? 'checked' : ''} />
          <label for="item${i}">${plate.text}</label>
         </li>
         `;
     }).join('')
 }
 
-function toggleDone(e){
-    if(!e.target.matches('input'))return; // skip this unless it is an input
-    const element = e.target 
+function toggleDone(e) {
+    if (!e.target.matches('input')) return; // skip this unless it is an input
+    const element = e.target
     const index = element.dataset.index;
     items[index].done = !items[index].done;
     localStorage.setItem('items', JSON.stringify(items));
-    console.log(items);
     populateList(items, itemsList);
 }
 
 addItems.addEventListener('submit', addItem);
-itemsList.addEventListener('click',toggleDone)
+itemsList.addEventListener('click', toggleDone)
 
 populateList(items, itemsList);
 
